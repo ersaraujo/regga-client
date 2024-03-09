bca30dd13f4ec677da21ea843404befd53506c42
Elisson Rodrigo <ersa@cin.ufpe.br>
2024-03-09 16:14:37 -0300

    update main

--- src/main.py
+++ src/main.py
@@ -6,10 +6,10 @@ import time as t
 
 def main():
     # print("Hello, World!")
-    # print("Humidity Sensor: ", hSensor().read())
     while True:
         # print("Light Sensor: ", lSensor().read())
-        print("Temperature Sensor: ", tSensor().read())
+        # print("Temperature Sensor: ", tSensor().read())
+        print("Humidity Sensor: ", hSensor().read())
         t.sleep(2.)
 
 if __name__ == "__main__":
