60bc49fc1502d582627abb468b2e614d2e386209
Elisson Rodrigo <ersa@cin.ufpe.br>
2024-03-09 16:31:13 -0300

    socket client

--- src/main.py
+++ src/main.py
@@ -15,28 +15,14 @@
 # if __name__ == "__main__":
 #     main()
 
-
-import socket
+from socketConnection import SocketClient
 
 def main():
-    # Configurar o servidor
-    host = 'localhost'  # ou '0.0.0.0' para aceitar conexões de qualquer IP
-    port = 12345        # Número da porta
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((host, port))
-    server_socket.listen(1)
-
-    print("Aguardando conexão do cliente...")
-    conn, addr = server_socket.accept()
-    print("Conexão estabelecida com", addr)
-
-    # Enviar dados para o cliente
-    data_to_send = "Hello, client!"
-    conn.sendall(data_to_send.encode())
-
-    # Fechar a conexão
-    conn.close()
-    server_socket.close()
+    
+    client = SocketClient('172.22.66.252', 12345)
+    client.connect()
+    client.send("Hello, World!")
+    client.close()
 
 if __name__ == "__main__":
     main()
---
+++ src/socketConnection/__init__.py
@@ -0,0 +1,1 @@
+from .socketConnection import SocketClient
