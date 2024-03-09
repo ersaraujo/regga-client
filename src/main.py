561af0c2e110ff60924794c8b0641b84a2fc64fb
Elisson Rodrigo <ersa@cin.ufpe.br>
2024-03-09 15:03:11 -0300

    update main

--- src/main.py
+++ src/main.py
@@ -6,7 +6,8 @@ def main():
     # print("Hello, World!")
     # print("Humidity Sensor: ", hSensor().read())
     # print("Temperature Sensor: ", tSensor().read())
-    print("Light Sensor: ", lSensor().read())
+    while True:
+        print("Light Sensor: ", lSensor().read())
 
 if __name__ == "__main__":
     main()
