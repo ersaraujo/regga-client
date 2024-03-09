87e5f67b60f0fefb9c6ca4d0cceebd93c1b21069
Elisson Rodrigo <ersa@cin.ufpe.br>
2024-03-09 16:13:33 -0300

    try sensor

--- src/humiditySensor/humiditySensor.py
+++ src/humiditySensor/humiditySensor.py
@@ -1,6 +1,16 @@
+import RPi.GPIO as GPIO
+
+DO_PIN = 21
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(DO_PIN, GPIO.IN)
+
 class HumiditySensor:
     def __init__(self, humiditySensorAdapter):
-        self.humiditySensorAdapter = humiditySensorAdapter
-
-    def getHumidity(self):
-        return self.humiditySensorAdapter.getHumidity()
+        pass
+        
+    def read(self):
+        self.sHumidity = GPIO.input(DO_PIN)
+        if self.sHumidity == GPIO.LOW:
+            return "Sensor Detected"
+        else:
+            return "No Senhor Detected"
