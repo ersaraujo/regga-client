85ee7a6561b3cee90b059c0d98002cd7f2261dd3
Elisson Rodrigo <ersa@cin.ufpe.br>
2024-03-10 11:39:19 -0300

    wip

--- src/main.py
+++ src/main.py
@@ -4,9 +4,15 @@ import time as t
 import json
 import re
 
+def data_parser(data):
+    data_len = len(data)
+    for i in range(data_len):
+        data[i] = re.search(r"'(.*?)\\", data[i]).group(1)
+    return data
+
 def main():
     ws = websocket.WebSocket()
-    ws.connect("ws://young-windows-beam.loca.lt/ws")
+    # ws.connect("ws://young-windows-beam.loca.lt/ws")
     with serial.Serial('/dev/ttyACM1', 9600) as arduino:
         t.sleep(0.1)
         if arduino.isOpen():
@@ -16,9 +22,14 @@ def main():
                     if arduino.in_waiting > 0:
                         answer=str(arduino.readline())
                         dataList = answer.split("X")
-                        print(dataList)
-                        data1 = re.search(r"'(.*?)\\", dataList[0].group(1))
-                        print(data1.group(2))
+                        data = data_parser(dataList)
+                        temp = {
+                            "temperature": data[0],
+                            "humidity": data[1],
+                            "luminosity": data[2],
+                            "humidityGround": data[3]
+                        }
+                        print(temp)
                         # print("{}".format(arduino.readline()))
                         # ws.send("{}".format(arduino.readline()))
                         # ws.send("Update data")
